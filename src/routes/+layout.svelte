26ad24b2c4fed1aaedc3b324ad41d2a221e9293a
dgjungle_c <dgjungle_c@163.com>
2023-05-27 17:58:36 +0800

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,4 +5,23 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	function scrollHandler(event: UIEvent & { currentTarget: EventTarget & HTMLDivElement }) {
+		console.log(event.currentTarget.scrollTop);
+	}
 </script>
+
+<AppShell on:scroll={scrollHandler}>
+	<!-- (header) -->
+	<svelte:fragment slot="sidebarLeft">
+		<div id="sidebar-left" class="hidden lg:block w-[10vw] bg-slate-700 h-full">Sidebar</div>
+	</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
