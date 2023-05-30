3c4bedf8fd450d8926b7b479d029f003df023145
dgjungle_c <dgjungle_c@163.com>
2023-05-30 14:51:08 +0800

    账本布局

---
+++ src/routes/cashbook/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageData } from '../cashbook/$types';
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	export let data: PageData;
+	let cashbookType: string = 'favor';
+</script>
+
+<TabGroup>
+	<Tab bind:group={cashbookType} name="favor" value={'favor'}>人情</Tab>
+	<Tab bind:group={cashbookType} name="consume" value={'consume'}>消费</Tab>
+	<!-- Tab Panels --->
+	<svelte:fragment slot="panel">
+		{#if cashbookType === 'favor'}
+			(tab panel 1 contents)
+		{:else if cashbookType === 'consume'}
+			(tab panel 2 contents)
+		{/if}
+	</svelte:fragment>
+</TabGroup>
