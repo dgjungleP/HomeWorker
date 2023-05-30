4e804c115b42e49ffe8b4ebabce2789d18dcf7a6
dgjungle_c <dgjungle_c@163.com>
2023-05-30 18:18:44 +0800

    消费单布局

--- src/routes/cashbook/+layout.svelte
+++ src/routes/cashbook/+layout.svelte
@@ -3,17 +3,25 @@
 	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 	export let data: PageData;
 	let cashbookType: string = 'favor';
+	const time: number[] = [];
+	for (let i = 0; i < 30; i++) {
+		time.push(i);
+	}
 </script>
 
-<TabGroup>
+<TabGroup regionPanel="p-4 flex-[1] pt-0" class=" flex flex-col h-full  ">
 	<Tab bind:group={cashbookType} name="favor" value={'favor'}>人情</Tab>
 	<Tab bind:group={cashbookType} name="consume" value={'consume'}>消费</Tab>
 	<!-- Tab Panels --->
 	<svelte:fragment slot="panel">
-		{#if cashbookType === 'favor'}
-			(tab panel 1 contents)
-		{:else if cashbookType === 'consume'}
-			(tab panel 2 contents)
-		{/if}
+		<div class=" card p-4 grid grid-cols-10 variant-ghost h-full grid-rows-5 gap-3">
+			{#if cashbookType === 'favor'}
+				{#each time as idx}
+					<div class="card p-4 card-hover h-full">Basic</div>
+				{/each}
+			{:else if cashbookType === 'consume'}
+				(tab panel 2 contents)
+			{/if}
+		</div>
 	</svelte:fragment>
 </TabGroup>
