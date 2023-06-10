3ad2a973cb323964f15bac748da8f37ed8431151
dgjungle_c <dgjungle_c@163.com>
2023-06-10 13:38:22 +0800

    搜索功能添加

--- src/routes/relation/+page.svelte
+++ src/routes/relation/+page.svelte
@@ -9,14 +9,16 @@
 	import type { DrawerSettings } from '@skeletonlabs/skeleton';
 
 	export let data: PageData;
+	const NODE_PREFIX = 'node-';
+	const MIN_MARGIN = 80;
 	var g = new dagre.graphlib.Graph();
 	$: nodes = g.nodes();
 	$: lines = new Array<Line>();
 	$: marginRightNeed = 0;
 	$: marginButtonNeed = 0;
+	$: searchUser = '';
 
 	onMount(() => {
-		// 为图像标签设置一个空对象
 		g.setGraph({
 			nodesep: 25,
 			rankdir: 'TB',
@@ -25,7 +27,6 @@
 			ranksep: 80
 		});
 
-		// 默认为每个边分配一个新的标签对象
 		g.setDefaultEdgeLabel(function () {
 			return {};
 		});
@@ -40,6 +41,7 @@
 			lines.push(...getLines(e, g));
 		});
 		nodes = g.nodes();
+		// 模拟画布
 		marginRightNeed = g.graph().width!.valueOf() + 200;
 		marginButtonNeed = g.graph().height!.valueOf() + 200;
 	});
@@ -58,15 +60,127 @@
 		};
 		drawerStore.open(drawerSetting);
 	}
+
+	function handleSearch(event: KeyboardEvent & { currentTarget: EventTarget & HTMLInputElement }) {
+		if (searchUser && event.key == 'Enter') {
+			const currentSelectedNode = NODE_PREFIX + searchUser;
+			const layout = document.getElementById('graph-layout')!;
+			const currentNode = document.getElementById(currentSelectedNode);
+			if (currentNode == null) {
+				return;
+			}
+			const currentWind = {
+				top: layout.scrollTop,
+				left: layout.scrollLeft,
+				bottom: layout.clientHeight + layout.scrollTop,
+				right: layout.clientWidth + layout.scrollLeft
+			};
+
+			if (
+				!checkInSafeArea(
+					currentWind,
+					currentNode.offsetTop,
+					currentNode.offsetLeft,
+					currentNode.clientHeight,
+					currentNode.clientWidth
+				)
+			) {
+				const moveTop = Math.max(
+					calculateMarginTop(
+						currentWind.top,
+						currentWind.bottom,
+						currentNode.offsetTop,
+						currentNode.clientHeight
+					),
+					0
+				);
+				const moveLeft = Math.max(
+					calculateMarginLeft(
+						currentWind.left,
+						currentWind.right,
+						currentNode.offsetLeft,
+						currentNode.clientWidth
+					),
+					0
+				);
+				console.log(currentWind);
+				console.log(currentNode.offsetLeft, moveLeft);
+				console.log(currentNode.offsetTop, moveTop);
+
+				layout.scrollTo({
+					top: moveTop,
+					left: moveLeft
+				});
+			}
+		}
+	}
+
+	function checkInSafeArea(
+		currentWind: { top: number; left: number; bottom: number; right: number },
+		offsetTop: number,
+		offsetLeft: number,
+		height: number,
+		width: number
+	): boolean {
+		return (
+			currentWind.top < offsetTop - MIN_MARGIN &&
+			currentWind.left < offsetLeft - MIN_MARGIN &&
+			currentWind.bottom > offsetTop + height + MIN_MARGIN &&
+			currentWind.right > offsetLeft + width + MIN_MARGIN
+		);
+	}
+
+	function calculateMarginTop(start: number, end: number, current: number, base: number): number {
+		const random = Math.random() * 10;
+		if (current <= start + MIN_MARGIN) {
+			return current - (MIN_MARGIN + random);
+		} else if (current + base >= end - MIN_MARGIN) {
+			return current + MIN_MARGIN + base + random;
+		}
+		return start;
+	}
+	function calculateMarginLeft(start: number, end: number, current: number, base: number): number {
+		const random = Math.random() * 10;
+		if (current <= start + MIN_MARGIN) {
+			return current - (MIN_MARGIN + random);
+		} else if (current + base >= end - MIN_MARGIN) {
+			return start + MIN_MARGIN + base + random + (current - end);
+		}
+		return start;
+	}
 </script>
 
-<div class=" relative w-full h-full overflow-scroll">
+<div
+	class="input-group input-group-divider grid-cols-[auto_1fr_auto] fixed w-1/5 rounded-full z-[999] top-7 ml-8 opacity-25 hover:opacity-100 focus:opacity-100"
+>
+	<div class="input-group-shim">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+			/>
+		</svg>
+	</div>
+	<input type="search" placeholder="Search..." on:keypress={handleSearch} bind:value={searchUser} />
+</div>
+
+<div id="graph-layout" class=" relative w-full h-full overflow-scroll scroll-smooth">
 	{#each nodes as node}
 		<div
+			id={NODE_PREFIX + node}
 			transition:fade
 			class="card absolute bg-orange-400 card-hover grid grid-cols-2 grid-rows-3 gap-1 pt-4 px-4"
 			style="left:{g.node(node).x}px;top:{g.node(node).y}px; width:{g.node(node)
 				.width}px;height:{g.node(node).height}px"
+			class:selected-node={searchUser == node}
 			on:click={() => showDraw(node)}
 			on:keydown
 		>
