3e62b02bf8a4aa5a7bff45405c273742126cce7c
dgjungle_c <dgjungle_c@163.com>
2023-06-15 10:03:34 +0800

    保险单模板

--- src/component/Modal/InsureModal.svelte
+++ src/component/Modal/InsureModal.svelte
@@ -9,6 +9,36 @@
 </script>
 
 {#if $modalStore[0]}
-	<div class={cBase}>Hello</div>
+	<div class=" flex flex-col gap-6">
+		<div class="{cBase} grid gap-2 grid-cols-10">
+			<div class=" col-span-6 border border-black p-3">
+				<span>保险公司:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-2 col-end-10 border border-black">
+				<span>投保产品:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-2 col-end-7 border border-black">
+				<span>保单号:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-2 col-end-4 border border-black">
+				<span>投保金额:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-5 col-end-7 border border-black">
+				<span>投保分数:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-8 col-end-10 row-start-4 row-end-6 border border-black">
+				<span>保额:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-2 col-end-10 border border-black">
+				<span>保险时间:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-2 col-end-5 border border-black">
+				<span>投保人:</span><span>XXXX</span>
+			</div>
+			<div class=" col-start-7 col-end-10 border border-black">
+				<span>受益人:</span><span>XXXX</span>
+			</div>
+		</div>
+	</div>
 {/if}
 <slot />
